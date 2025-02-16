5e416fc222068dfe0d1ffbc3cb36e633fe9f67a5
osama <ghadmios235@gmail.com>
2025-02-16 13:11:25 +0000

    alpha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - hi
+ *
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+
+char l;
+
+for (l = 'a'; l <= 'z'; l++)
+putchar(l);
+
+putchar('\n');
+return (0);
+}
