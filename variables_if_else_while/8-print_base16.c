ec43cc78966e6c86792c81355ecc9327db08fc4d
osama <ghamdios235@gmail.com>
2025-02-17 14:12:54 +0000

    0 to f

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - hi my name is osama.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+int n;
+
+char l;
+
+for (n = 0; n <= 9; n++)
+putchar(n + '0');
+
+for (l = 'a'; l <= 'f'; l++)
+putchar(l);
+
+putchar('\n');
+
+return (0);
+
+}
