d392172df0cab586b87379a5922fe142b0889ea9
osama <ghamdios235@gmail.com>
2025-02-17 08:28:57 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - hi
+ *
+ *
+ * Return : always 0.
+ */
+
+int main(void)
+{
+char l;
+
+char L;
+
+for (l = 'a'; l <= 'z'; l++)
+putchar(l);
+
+for (L = 'A'; L <= 'Z'; L++)
+putchar(L);
+
+putchar('\n');
+
+return (0);
+
+}
