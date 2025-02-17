5e0ed6473b8caaa558dbce3769437829da99a49e
osama <ghamdios235@gmail.com>
2025-02-17 13:48:24 +0000

    1 to 9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0 to 9.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i <= 9; i++)
+putchar(i + '0');
+
+putchar('\n');
+
+return (0);
+
+}
