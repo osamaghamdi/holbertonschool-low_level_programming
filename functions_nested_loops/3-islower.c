221bef8508fa454a21285bdf536ea780d0cbad1c
osama <ghamdios235@gmail.com>
2025-02-20 20:36:54 +0000

    011

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - check if character is in lowercase
+ *@c: input character
+ * Return: int
+ *
+ */
+
+int _islower(int c)
+{
+
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
