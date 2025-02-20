6ffb63e33d5fed50d4498bf0e89c8fe8ef7ef532
osama <ghamdios235@gmail.com>
2025-02-20 20:25:12 +0000

    hi

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - from a to z
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+char a;
+int i = 1;
+
+while (i <= 10)
+{
+for (a = 'a'; c <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+i++;
+}
+}
