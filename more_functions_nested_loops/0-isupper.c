93a4d00291633c4b2fff23ad3fc6d3a135085bb6
osama <ghamdios235@gmail.com>
2025-02-24 07:40:34 +0000

    upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @n: character
+ *
+ *Return: 1 if uppercase, 0 if not.
+ */
+
+int _isupper(int n)
+{
+
+if (n >= 'A' && n <= 'Z')
+return (1);
+
+return (0);
+
+}
