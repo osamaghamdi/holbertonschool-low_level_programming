e059fc5016c908900448d9c0b71934356803c3eb
osama <ghadmios235@gmail.com>
2025-02-25 07:06:09 +0000

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - hi
+ * @c: character
+ *
+ * Return: 1 if digit, 0 if not
+ */
+
+int _isdigit(int c)
+{
+
+if (c >= '0' && c <= '9')
+return (1);
+
+return (0);
+
+}
