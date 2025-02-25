52882e4185f94f2899fe2af38a0c11f4cc6342b0
osama <ghadmios235@gmail.com>
2025-02-25 08:20:39 +0000

    0 - 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten time
+ *
+ *Returm: void
+ */
+
+void more_numbers(void)
+{
+
+int a, b;
+
+for (a = 0; a <= 10; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 9)
+_putchar('1');
+_putchar((b % 10) + '0');
+}
+_putchar('\n');
+}
+}
