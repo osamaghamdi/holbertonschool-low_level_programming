c6a66cfb911d9c1acaf7397ed4356013bc9fbc0c
osama <ghadmios235@gmail.com>
2025-02-25 08:58:16 +0000

    ____

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - hi
+ *
+ * @n: n
+ *
+ * return: void
+ */
+
+void print_line(int n)
+{
+
+int line;
+
+if (n > 0)
+{
+
+for (line = 0; line < n; line++)
+_putchar('_');
+
+}
+
+_putchar('\n');
+
+}
