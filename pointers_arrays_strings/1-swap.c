922ce6634d655bd07a960ef0ebba06f80359cfdd
osama <ghamdios235@gmail.com>
2025-02-27 07:58:11 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value
+ *
+ * @a: p1
+ * @b: p2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int x = *a;
+
+*a = *b;
+*b = x;
+
+}
