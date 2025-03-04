2b46f6ba3e1c00aa648cf323d2d0b7bca37e4898
osama <ghamdios235@gmail.com>
2025-03-04 11:35:37 +0000

    new

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @dest: destination
  * @src: source
  * @n: bytes
- * Returne: pointer
+ * Return: pointer
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -17,4 +17,5 @@ unsigned int i;
 for (i = 0; i < n; i++)
 dest[i] = src[i];
 return (dest);
+
 }
