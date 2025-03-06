9c2abb5aebbc951d7d8921ccfe8958c1096a5ce6
osama <ghamdios235@gmail.com>
2025-03-06 10:46:42 +0000

    prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * is_prime - hi
+ * @n1: number
+ * @n2: number
+ * Return: number
+ */
+
+int is_prime(int n1, int n2)
+{
+
+if (n2 * n2 > n1)
+return (1);
+if (n1 % n2 == 0)
+return (0);
+
+return (is_prime(n1, n2 + 1));
+
+}
+
+/**
  * is_prime_number - print 1 for prime number
  * @n: number
  * Return: 1 or 0
@@ -9,18 +28,9 @@
 int is_prime_number(int n)
 {
 
-int i;
-
 if (n < 2)
 return (0);
 
-if (n == 2)
-return (1);
+return (is_prime(n, 2));
 
-for (i = 2; i * i <= n; i++)
-{
-if (n % i == 0)
-return (0);
-}
-return (1);
 }
