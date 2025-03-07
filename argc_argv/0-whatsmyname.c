fa91137480478b1f19c728ce7a4eb172fe96891f
osama <ghamdios235@gmail.com>
2025-03-07 19:14:48 +0000

    my_name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+
+}
