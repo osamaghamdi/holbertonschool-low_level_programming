a8df960f353f05d8360028ccf611d798f3aa99af
osama <ghamdios235@gmail.com>
2025-04-04 11:46:29 +0000

    0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,28 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all element of list
- * @h: the list
- * Return: number of nodes in h
+ * print_dlistint_t - prints all element of list
+ * @h: the head of list
+ * Return: number of nodes in list
  */
 
-size_t print_list(const list_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
-
 size_t node = 0;
+
 while (h)
 {
-
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-
 node++;
-h = h->next
-
+printf("%d\n", h->n);
+h = h->next;
 }
 
 return (node);
-
 }
